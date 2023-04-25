135e44727748a7350cef492330d95b6d18d1a69c
Cale Teeter <cale.teeter@microsoft.com>
2023-04-25 13:47:22 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,13 +18,13 @@ az login --identity --username "${managedIdentity}"
 
 # get credentials for kubectl used for data plane operations
 az aks install-cli
-# az aks get-credentials --name "${aksClusterName}" --resource-group "${resourceGroupName}"
+az aks get-credentials --name "${aksClusterName}" --resource-group "${resourceGroupName}"
 
 # replicate images from customer Digital Asset repos
-# az acr import --name "${acrName}" --source "digitalasset-${company}-docker.jfrog.io/canton-enterprise:${version}" --username "${username}" --password "${password}"
-# az acr import --name "${acrName}" --source "digitalasset-${company}-docker.jfrog.io/http-json:${version}"         --username "${username}" --password "${password}"
-# az acr import --name "${acrName}" --source "digitalasset-${company}-docker.jfrog.io/trigger-service:${version}"   --username "${username}" --password "${password}"
-# az acr import --name "${acrName}" --source "digitalasset-${company}-docker.jfrog.io/oauth2-middleware:${version}" --username "${username}" --password "${password}"
+az acr import --name "${acrName}" --source "digitalasset-${company}-docker.jfrog.io/canton-enterprise:${version}" --username "${username}" --password "${password}"
+az acr import --name "${acrName}" --source "digitalasset-${company}-docker.jfrog.io/http-json:${version}"         --username "${username}" --password "${password}"
+az acr import --name "${acrName}" --source "digitalasset-${company}-docker.jfrog.io/trigger-service:${version}"   --username "${username}" --password "${password}"
+az acr import --name "${acrName}" --source "digitalasset-${company}-docker.jfrog.io/oauth2-middleware:${version}" --username "${username}" --password "${password}"
 
 # ensure the preview bits can be used with prompt in UI
 # az config set extension.use_dynamic_install=yes_without_prompt
