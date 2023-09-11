565eb05e8eda2d858e852f22a59e6709f2d36d7c
Cale Teeter <cale.teeter@microsoft.com>
2023-09-11 12:40:33 -0400

    Temp for testing postgres with private cluster

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ serverName="${9}"
 administratorLogin="${10}"
 administratorLoginPassword="${11}"
 
-artifactsBaseUrl="https://raw.githubusercontent.com/digital-asset/daml-azure-deployment-templates/main"
+artifactsBaseUrl="https://raw.githubusercontent.com/caleteeter/daml-azure-deployment-templates/main/"
 
 # login
 az login --identity --username "${managedIdentity}"
@@ -34,3 +34,13 @@ az config set extension.use_dynamic_install=yes_without_prompt
 # install the psql client
 apk --no-cache add postgresql-client
 
+# create database objects
+wget -O postgresql.sql $artifactsBaseUrl/scripts/postgresql.sql
+
+# update tokens in script with real values
+# shellcheck disable=SC2002
+dbPass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+sed -i "s/DB_PASS/${dbPass}/g" postgresql.sql
+sed -i "s/DB_ADMIN/${administratorLogin}/g" postgresql.sql
+
+psql "host=${serverName}.postgres.database.azure.com port=5432 dbname=postgres user=${administratorLogin} password=${administratorLoginPassword} sslmode=require" -a -f "postgresql.sql"
