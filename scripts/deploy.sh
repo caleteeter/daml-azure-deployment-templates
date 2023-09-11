5eeb8a0144762416a1e9263f2b3a2b4a1a4d6502
Cale Teeter <cale.teeter@microsoft.com>
2023-09-11 18:09:08 -0400

    Testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -39,8 +39,8 @@ wget -O postgresql.sql "${artifactsBaseUrl}/scripts/postgresql.sql"
 
 # update tokens in script with real values
 # shellcheck disable=SC2002
-#dbPass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-#sed -i "s/DB_PASS/${dbPass}/g" postgresql.sql
-#sed -i "s/DB_ADMIN/${administratorLogin}/g" postgresql.sql
+dbPass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+sed -i "s/DB_PASS/${dbPass}/g" postgresql.sql
+sed -i "s/DB_ADMIN/${administratorLogin}/g" postgresql.sql
 
 # psql "host=${serverName}.postgres.database.azure.com port=5432 dbname=postgres user=${administratorLogin} password=${administratorLoginPassword} sslmode=require" -a -f "postgresql.sql"
