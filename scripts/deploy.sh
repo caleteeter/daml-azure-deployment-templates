3e4ecf0c3a577fa5a5a7a63bfd0a265bf655afd0
Cale Teeter <cale.teeter@microsoft.com>
2023-09-15 16:22:27 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -94,7 +94,10 @@ helm replace-values-env -f values/azure.yaml -u
 helm replace-values-env -f values/storage.yaml -u
 
 # install the helm charts
-helm install canton-domain digital-asset/canton-domain
-helm install canton-participant digital-asset/canton-participant
-helm install daml-http-json digital-asset/daml-http-json
-helm install daml-trigger digital-asset/daml-trigger
+# helm install canton-domain digital-asset/canton-domain
+# helm install canton-participant digital-asset/canton-participant
+# helm install daml-http-json digital-asset/daml-http-json
+# helm install daml-trigger digital-asset/daml-trigger
+
+# deployment
+helmfile -f helmfile.yaml -l 'default=true' apply --skip-deps
